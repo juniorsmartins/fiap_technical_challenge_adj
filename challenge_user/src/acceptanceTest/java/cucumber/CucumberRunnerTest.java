b20ce7ef6a54bfa93a37df04c9dedea716a9789c
juniorsmartins <junior.soares.martins@proton.me>
2025-03-29 17:01:08 -0300

    CHORE: configuração de banco de dados.

---
+++ challenge_user/src/acceptanceTest/java/cucumber/CucumberRunnerTest.java
@@ -0,0 +1,32 @@
+package cucumber;
+
+import br.com.fiap.tech.challenge_user.ChallengeUserApplication;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/acceptanceTest/resources/features", "classpath:features"},
+        glue = {"cucumber.config",
+                "cucumber.steps"},
+        plugin = {
+                "pretty",
+                "html:src/acceptanceTest/cucumber-reports/cucumber.html", // Relatório HTML
+                "json:src/acceptanceTest/cucumber-reports/cucumber.json" // Relatório JSON
+        },
+        monochrome = true,
+        snippets = CucumberOptions.SnippetType.UNDERSCORE,
+        dryRun = false // Defina como true para verificar se todos os steps estão implementados sem executar os testes
+)
+@CucumberContextConfiguration
+@SpringBootTest(
+        classes = ChallengeUserApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+public class CucumberRunnerTest {
+
+}
+
